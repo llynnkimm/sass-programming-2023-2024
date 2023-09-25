7777d582578ea86ac7511cd3a8cacaa9249d22b1
Lynn Kim <llynnkimm@gmail.com>
2023-09-25 09:47:57 -0700

    Added lists and modules

---
+++ Notes/Lists.md
@@ -0,0 +1,20 @@
+A list in python is a collection of items
+We can store different values in a list
+Order matters in a list
+
+# Creating a List
+To make a list we use brackets [/] to surround out list
+We seperate out the items using 
+
+```python
+some_list = ["John", "Tim", "Sara"]
+```
+
+# Access Elements in a List
+We can grab things from lists using the bracket notation
+In our example above, if I wanted to access "Tim", I would do the following 
+
+```python
+some_list[1] # "Lynn"
+```
+
--- Notes/chatbot.py
+++ Notes/chatbot.py
@@ -2,18 +2,30 @@
 # Author: Lynn 
 # Date: 20 September 2023
 
+import random
+import time
+
 # Greet the user
 print("Hello there! 🤖")
+# Pause for 1.5 seconds
+time.sleep(1)
 print("I'm a crude Chatbot, here to talk to you.")
+time.sleep(1)
 
 # Get the user's name and store it in the variable
 user_name = input("So... What's your name? ")
 
+time.sleep(1)
+
 # Respond with the user's name in a friendly way
 print(f"It's good to meet you, {user_name}.")
 
+time.sleep(1)
+
 # Ask the user what their favourite food is
 favourite_food = input("I have a quick question... What is your favourite food? ")
+
+time.sleep(1)
 
 # Make a comment about their favourite food but NOT OVERLY REPETITIVE
 # Create a list of possible responses 
@@ -25,9 +37,12 @@ list_of_food_responses = [
     "Oh! that does sound pretty good right about now! 🤠"
 ]
 
+print(list_of_food_responses[2])
+
 # Choose one of those responses randomly
-import random
 random_food_response = random.choice(list_of_food_responses)
+
+time.sleep(1)
 
 # Print out that chosen response
 print(random_food_response)
